d0e32491250cbbe053ff6b78ac897c2d61f130fb
Renato Druzian <rendruzian@gmail.com>
2025-10-30 13:19:47 -0300

    added swagger configuration and initiate repository and services

--- server/server.go
+++ server/server.go
@@ -1,8 +1,13 @@
 package server
 
 import (
-	"github.com/rdruzian/challenge_interview/router"
 	"log"
+	"os"
+
+	"github.com/rdruzian/challenge_interview/database"
+	"github.com/rdruzian/challenge_interview/repository"
+	"github.com/rdruzian/challenge_interview/router"
+	"github.com/rdruzian/challenge_interview/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,7 +25,22 @@ func NewServer() Server {
 }
 
 func (s *Server) Run() {
-	r := router.ConfigRoutes(s.server)
+	// Modo somente Swagger: pula DB e serviços para facilitar preview/local
+	if os.Getenv("SWAG_ONLY") == "true" {
+		r := router.ConfigRoutes(s.server, nil)
+		log.Fatal(r.Run(":" + s.port))
+		return
+	}
 
+	// Initialize database
+	database.StartDB()
+	db := database.GetDatabase()
+
+	// Wire repository and service
+	repo := repository.NewDeviceRepository(db)
+	deviceService := service.NewDeviceService(repo)
+
+	// Configure routes with service and start server
+	r := router.ConfigRoutes(s.server, deviceService)
 	log.Fatal(r.Run(":" + s.port))
 }

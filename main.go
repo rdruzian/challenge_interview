e2ea879c2088c4f9eb10c4a1f9c92cf2695fa7f3
Renato Druzian <rendruzian@gmail.com>
2025-10-30 13:15:19 -0300

    added swagger configurations

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/rdruzian/challenge_interview/docs"
 	"github.com/rdruzian/challenge_interview/server"
 )
 
@@ -12,6 +13,13 @@ import (
 // @host            localhost:8080
 // @BasePath        /api/v1
 func main() {
+	// Configura metadados do Swagger em runtime (garante consistência)
+	docs.SwaggerInfo.Title = "challenge_interview"
+	docs.SwaggerInfo.Description = "API for challenge code interview, that I need to develop a CRUD syste"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost:8080"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+
 	// Create an instance of server
 	server := server.NewServer()
 	// Initiate a server

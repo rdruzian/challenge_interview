71eb00d390628f64501e9a7bebe113c515c210ae
Renato Druzian <rendruzian@gmail.com>
2025-10-30 15:59:06 -0300

    added id and CreatedAt in json

--- model/Device.go
+++ model/Device.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Device struct {
-	ID           int64     `json:"-" db:"id"`
+	ID           int64     `json:"id" gorm:"unique;primaryKey;autoIncrement"`
 	Name         string    `json:"name" db:"name"`
 	Brand        string    `json:"brand" db:"brand"`
 	State        string    `json:"state" db:"state"`
-	CreationDate time.Time `json:"-" db:"creation_date"`
+	CreationDate time.Time `json:"createdAt" db:"creation_date"`
 }
